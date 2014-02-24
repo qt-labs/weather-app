4c7c3e9676cdba791e026aecdad4fc8c6d7ab789
Liang Qi <liang.qi@digia.com>
2014-02-24 13:30:15 +0100

    Translation: Add Simplified Chinese
    Since Traditional Chinese is not available,
    adding Simplified Chinese as default language for
    one or the other detected (zh).
    
    Change-Id: I0262ee8054450babbc90b4eaeed0b4c6364fddb5
    Reviewed-by: Jing Bai <doris.bai@gmail.com>

---
+++ translations/QuickForecast_zh.ts
@@ -0,0 +1,121 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>ApplicationInfo</name>
+    <message>
+        <source>Error loading image - Host not found or unreachable</source>
+        <translation>加载图片错误 - 主机没有找到或者不可访问</translation>
+    </message>
+    <message>
+        <source>Waiting for cities, network may be slow...</source>
+        <translation>正在等待城市信息，网络也许有点慢...</translation>
+    </message>
+    <message>
+        <source>Network error: %1</source>
+        <translation>网络错误：%1</translation>
+    </message>
+</context>
+<context>
+    <name>BasicPage</name>
+    <message>
+        <source>Find City</source>
+        <translation>查找城市</translation>
+    </message>
+</context>
+<context>
+    <name>CitiesListModel</name>
+    <message>
+        <source>no city found</source>
+        <translation>没有找到城市</translation>
+    </message>
+</context>
+<context>
+    <name>CitiesPage</name>
+    <message>
+        <source>Cities</source>
+        <translation>城市</translation>
+    </message>
+    <message>
+        <source>No Cities</source>
+        <translation>没有城市</translation>
+    </message>
+</context>
+<context>
+    <name>CityModel</name>
+    <message>
+        <source>Network error: </source>
+        <translation>网络错误：</translation>
+    </message>
+    <message>
+        <source>No data at given url</source>
+        <translation>给定的URL中没有数据</translation>
+    </message>
+</context>
+<context>
+    <name>LongTermDayItem</name>
+    <message>
+        <source>m/s</source>
+        <translation>米/秒</translation>
+    </message>
+</context>
+<context>
+    <name>LongTermPage</name>
+    <message>
+        <source>10 Day Forecast</source>
+        <translation>10天预报</translation>
+    </message>
+    <message>
+        <source>Loading data...</source>
+        <translation>正在加载数据...</translation>
+    </message>
+    <message>
+        <source>Problem loading the data: </source>
+        <translation>加载数据出现问题：</translation>
+    </message>
+</context>
+<context>
+    <name>OneDayPage</name>
+    <message>
+        <source>Today</source>
+        <translation>今天</translation>
+    </message>
+    <message>
+        <source>Tomorrow</source>
+        <translation>明天</translation>
+    </message>
+</context>
+<context>
+    <name>OneDayZoomItem</name>
+    <message>
+        <source>Precipitation: </source>
+        <translation>降雨量：</translation>
+    </message>
+    <message>
+        <source>mm</source>
+        <translation>毫米</translation>
+    </message>
+    <message>
+        <source>Wind: </source>
+        <translation>风速：</translation>
+    </message>
+    <message>
+        <source>m/s</source>
+        <translation>米/秒</translation>
+    </message>
+</context>
+<context>
+    <name>WeatherModel</name>
+    <message>
+        <source>Can&apos;t parse the xml file</source>
+        <translation>无法解析XML文件</translation>
+    </message>
+</context>
+<context>
+    <name>utils</name>
+    <message>
+        <source>°C</source>
+        <translation>°C</translation>
+    </message>
+</context>
+</TS>
