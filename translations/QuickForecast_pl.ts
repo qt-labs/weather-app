6202575eb6f1449eb03508d5849b8c77fb0668ed
Caroline Chao <caroline.chao@digia.com>
2014-02-24 13:29:35 +0100

    Translation: Add Polish
    Change-Id: Ifdf938ba8d35788e628fe7988707e9e76a49c82f
    Reviewed-by: Jarek Kobus <jaroslaw.kobus@digia.com>

---
+++ translations/QuickForecast_pl.ts
@@ -0,0 +1,121 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="pl_PL">
+<context>
+    <name>ApplicationInfo</name>
+    <message>
+        <source>Error loading image - Host not found or unreachable</source>
+        <translation>Błąd wczytywania obrazka - Nie znaleziono hosta lub jest on nieosiągalny</translation>
+    </message>
+    <message>
+        <source>Waiting for cities, network may be slow...</source>
+        <translation>Oczekiwanie na miasta, połączenie sieciowe może być powolne...</translation>
+    </message>
+    <message>
+        <source>Network error: %1</source>
+        <translation>Błąd sieciowy: %1</translation>
+    </message>
+</context>
+<context>
+    <name>BasicPage</name>
+    <message>
+        <source>Find City</source>
+        <translation>Znajdź Miasto</translation>
+    </message>
+</context>
+<context>
+    <name>CitiesListModel</name>
+    <message>
+        <source>no city found</source>
+        <translation>nie znaleziono miasta</translation>
+    </message>
+</context>
+<context>
+    <name>CitiesPage</name>
+    <message>
+        <source>Cities</source>
+        <translation>Miasta</translation>
+    </message>
+    <message>
+        <source>No Cities</source>
+        <translation>Brak Miast</translation>
+    </message>
+</context>
+<context>
+    <name>CityModel</name>
+    <message>
+        <source>Network error: </source>
+        <translation>Błąd sieciowy: </translation>
+    </message>
+    <message>
+        <source>No data at given url</source>
+        <translation>Brak danych pod wskazanym url</translation>
+    </message>
+</context>
+<context>
+    <name>LongTermDayItem</name>
+    <message>
+        <source>m/s</source>
+        <translation>m/s</translation>
+    </message>
+</context>
+<context>
+    <name>LongTermPage</name>
+    <message>
+        <source>10 Days Forecast</source>
+        <translation>Prognoza na 10 dni</translation>
+    </message>
+    <message>
+        <source>Loading data...</source>
+        <translation>Ładowanie danych...</translation>
+    </message>
+    <message>
+        <source>Problem loading the data: </source>
+        <translation>Problem z ładowaniem danych: </translation>
+    </message>
+</context>
+<context>
+    <name>OneDayPage</name>
+    <message>
+        <source>Today</source>
+        <translation>Dziś</translation>
+    </message>
+    <message>
+        <source>Tomorrow</source>
+        <translation>Jutro</translation>
+    </message>
+</context>
+<context>
+    <name>OneDayZoomItem</name>
+    <message>
+        <source>Precipitation: </source>
+        <translation>Opady: </translation>
+    </message>
+    <message>
+        <source>mm</source>
+        <translation>mm</translation>
+    </message>
+    <message>
+        <source>Wind: </source>
+        <translation>Wiatr: </translation>
+    </message>
+    <message>
+        <source>m/s</source>
+        <translation>m/s</translation>
+    </message>
+</context>
+<context>
+    <name>WeatherModel</name>
+    <message>
+        <source>Can&apos;t parse the xml file</source>
+        <translation>Błąd parsowania pliku xml</translation>
+    </message>
+</context>
+<context>
+    <name>utils</name>
+    <message>
+        <source>°C</source>
+        <translation>°C</translation>
+    </message>
+</context>
+</TS>
