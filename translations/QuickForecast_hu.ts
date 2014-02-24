d4e8e82a3d63cd11b7ae171d933c80542ef2f783
Caroline Chao <caroline.chao@digia.com>
2014-02-24 13:28:04 +0100

    Translation: Add Hungarian
    Change-Id: I442d42a06b0cc37e394fb97eb8dc780fce11e44b
    Reviewed-by: Caroline Chao <caroline.chao@digia.com>

---
+++ translations/QuickForecast_hu.ts
@@ -0,0 +1,121 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="hu_HU">
+<context>
+    <name>ApplicationInfo</name>
+    <message>
+        <source>Error loading image - Host not found or unreachable</source>
+        <translation>Hiba a kép betöltése közben - A kiszolgáló nem érhető el</translation>
+    </message>
+    <message>
+        <source>Waiting for cities, network may be slow...</source>
+        <translation>Várakozás, lassú hálózat...</translation>
+    </message>
+    <message>
+        <source>Network error: %1</source>
+        <translation>Hálózati hiba: %1</translation>
+    </message>
+</context>
+<context>
+    <name>BasicPage</name>
+    <message>
+        <source>Find City</source>
+        <translation>Város Keresése</translation>
+    </message>
+</context>
+<context>
+    <name>CitiesListModel</name>
+    <message>
+        <source>no city found</source>
+        <translation>város nem található</translation>
+    </message>
+</context>
+<context>
+    <name>CitiesPage</name>
+    <message>
+        <source>Cities</source>
+        <translation>Városok</translation>
+    </message>
+    <message>
+        <source>No Cities</source>
+        <translation>Nincsenek városok</translation>
+    </message>
+</context>
+<context>
+    <name>CityModel</name>
+    <message>
+        <source>Network error: </source>
+        <translation>Hálózati hiba: </translation>
+    </message>
+    <message>
+        <source>No data at given url</source>
+        <translation>Nincs adat az adott címen</translation>
+    </message>
+</context>
+<context>
+    <name>LongTermDayItem</name>
+    <message>
+        <source>m/s</source>
+        <translation>m/s</translation>
+    </message>
+</context>
+<context>
+    <name>LongTermPage</name>
+    <message>
+        <source>10 Day Forecast</source>
+        <translation>10 Napos Előrejelzés</translation>
+    </message>
+    <message>
+        <source>Loading data...</source>
+        <translation>Betöltés...</translation>
+    </message>
+    <message>
+        <source>Problem loading the data: </source>
+        <translation>Hiba az adatok betöltésénél: </translation>
+    </message>
+</context>
+<context>
+    <name>OneDayPage</name>
+    <message>
+        <source>Today</source>
+        <translation>Ma</translation>
+    </message>
+    <message>
+        <source>Tomorrow</source>
+        <translation>Holnap</translation>
+    </message>
+</context>
+<context>
+    <name>OneDayZoomItem</name>
+    <message>
+        <source>Precipitation: </source>
+        <translation>Csapadék: </translation>
+    </message>
+    <message>
+        <source>mm</source>
+        <translation>mm</translation>
+    </message>
+    <message>
+        <source>Wind: </source>
+        <translation>Szél: </translation>
+    </message>
+    <message>
+        <source>m/s</source>
+        <translation>m/s</translation>
+    </message>
+</context>
+<context>
+    <name>WeatherModel</name>
+    <message>
+        <source>Can&apos;t parse the xml file</source>
+        <translation>Érvénytelen xml file</translation>
+    </message>
+</context>
+<context>
+    <name>utils</name>
+    <message>
+        <source>°C</source>
+        <translation>°C</translation>
+    </message>
+</context>
+</TS>
