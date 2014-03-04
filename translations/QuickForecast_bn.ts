93af447a103816bf5a3dc42f6d856e499a63388e
Caroline Chao <caroline.chao@digia.com>
2014-03-04 09:37:56 +0100

    Translations: Add Bengali and Hindi
    Change-Id: I8b0bbf7bb0510b2430b2ef0f1d52d59dfdf74ea9
    Reviewed-by: Venugopal Shivashankar <venugopal.shivashankar@digia.com>
    Reviewed-by: Indrajit Tapadar <indrajit.tapadar@digia.com>
    Reviewed-by: Caroline Chao <caroline.chao@digia.com>

---
+++ translations/QuickForecast_bn.ts
@@ -0,0 +1,121 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="bn_IN">
+<context>
+    <name>ApplicationInfo</name>
+    <message>
+        <source>Error loading image - Host not found or unreachable</source>
+        <translation>ইমেজ লোডিং এ ত্রুটি - হোস্ট পাওয়া যাচ্ছে না</translation>
+    </message>
+    <message>
+        <source>Waiting for cities, network may be slow...</source>
+        <translation>শহরের অনুসন্ধান চোলছে, নেটওয়ার্ক ধীর হতে পারে ...</translation>
+    </message>
+    <message>
+        <source>Network error: %1</source>
+        <translation>নেটওয়ার্ক এ ত্রুটি: %1</translation>
+    </message>
+</context>
+<context>
+    <name>BasicPage</name>
+    <message>
+        <source>Find City</source>
+        <translation>শহর খুঁজুন</translation>
+    </message>
+</context>
+<context>
+    <name>CitiesListModel</name>
+    <message>
+        <source>no city found</source>
+        <translation>শহর পাওয়া যাইনি</translation>
+    </message>
+</context>
+<context>
+    <name>CitiesPage</name>
+    <message>
+        <source>Cities</source>
+        <translation>শহরগুলি</translation>
+    </message>
+    <message>
+        <source>No Cities</source>
+        <translation>কোনো শহর নেই</translation>
+    </message>
+</context>
+<context>
+    <name>CityModel</name>
+    <message>
+        <source>Network error: </source>
+        <translation>নেটওয়ার্ক ত্রুটি: </translation>
+    </message>
+    <message>
+        <source>No data at given url</source>
+        <translation>প্রদত্ত URL এ কোন তথ্য নেই</translation>
+    </message>
+</context>
+<context>
+    <name>LongTermDayItem</name>
+    <message>
+        <source>m/s</source>
+        <translation>মি / সেকেন্ড</translation>
+    </message>
+</context>
+<context>
+    <name>LongTermPage</name>
+    <message>
+        <source>10 Day Forecast</source>
+        <translation>১০ দিনের পূর্বাভাস</translation>
+    </message>
+    <message>
+        <source>Loading data...</source>
+        <translation>তথ্য লোড হচ্ছে...</translation>
+    </message>
+    <message>
+        <source>Problem loading the data: </source>
+        <translation>তথ্য লোডিং এ সমস্যা: </translation>
+    </message>
+</context>
+<context>
+    <name>OneDayPage</name>
+    <message>
+        <source>Today</source>
+        <translation>আজ</translation>
+    </message>
+    <message>
+        <source>Tomorrow</source>
+        <translation>আগামীকাল</translation>
+    </message>
+</context>
+<context>
+    <name>OneDayZoomItem</name>
+    <message>
+        <source>Precipitation: </source>
+        <translation>বৃষ্টিপাত: </translation>
+    </message>
+    <message>
+        <source>mm</source>
+        <translation>মিমি</translation>
+    </message>
+    <message>
+        <source>Wind: </source>
+        <translation>বায়ু: </translation>
+    </message>
+    <message>
+        <source>m/s</source>
+        <translation>মি / সেকেন্ড</translation>
+    </message>
+</context>
+<context>
+    <name>WeatherModel</name>
+    <message>
+        <source>Can&apos;t parse the xml file</source>
+        <translation>XML ফাইল বিশ্লেষণ করতে পারা যাছে না </translation>
+    </message>
+</context>
+<context>
+    <name>utils</name>
+    <message>
+        <source>°C</source>
+        <translation>° সেঃ</translation>
+    </message>
+</context>
+</TS>
---
+++ translations/QuickForecast_hi.ts
@@ -0,0 +1,121 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="hi_IN">
+<context>
+    <name>ApplicationInfo</name>
+    <message>
+        <source>Error loading image - Host not found or unreachable</source>
+        <translation>छवि लोड करने में गलती - होस्ट नहीं पाया गया या संपर्क से बाहर</translation>
+    </message>
+    <message>
+        <source>Waiting for cities, network may be slow...</source>
+        <translation>शहर की डेटा के लिए प्रतीक्षा कर रहा है,नेटवर्क धीमा हो सकता है ...</translation>
+    </message>
+    <message>
+        <source>Network error: %1</source>
+        <translation>नेटवर्क में गलती : %1</translation>
+    </message>
+</context>
+<context>
+    <name>BasicPage</name>
+    <message>
+        <source>Find City</source>
+        <translation>शहर पाता लगाऊ</translation>
+    </message>
+</context>
+<context>
+    <name>CitiesListModel</name>
+    <message>
+        <source>no city found</source>
+        <translation>इस नाम से कोई शहर नहीं मिला</translation>
+    </message>
+</context>
+<context>
+    <name>CitiesPage</name>
+    <message>
+        <source>Cities</source>
+        <translation>शहरों</translation>
+    </message>
+    <message>
+        <source>No Cities</source>
+        <translation>कोई शहर नहीं </translation>
+    </message>
+</context>
+<context>
+    <name>CityModel</name>
+    <message>
+        <source>Network error: </source>
+        <translation>नेटवर्क में गलती : </translation>
+    </message>
+    <message>
+        <source>No data at given url</source>
+        <translation>दिए गए यूआरएल पर कोई डेटा नहीं</translation>
+    </message>
+</context>
+<context>
+    <name>LongTermDayItem</name>
+    <message>
+        <source>m/s</source>
+        <translation>मीटर प्रति सेकंड</translation>
+    </message>
+</context>
+<context>
+    <name>LongTermPage</name>
+    <message>
+        <source>10 Day Forecast</source>
+        <translation>१० दिनों का पूर्वानुमान</translation>
+    </message>
+    <message>
+        <source>Loading data...</source>
+        <translation>डेटा लोड हो रहा है...</translation>
+    </message>
+    <message>
+        <source>Problem loading the data: </source>
+        <translation>डेटा लोड करने में समस्या / रूकावट: </translation>
+    </message>
+</context>
+<context>
+    <name>OneDayPage</name>
+    <message>
+        <source>Today</source>
+        <translation>आज</translation>
+    </message>
+    <message>
+        <source>Tomorrow</source>
+        <translation>कल</translation>
+    </message>
+</context>
+<context>
+    <name>OneDayZoomItem</name>
+    <message>
+        <source>Precipitation: </source>
+        <translation>वर्षा: </translation>
+    </message>
+    <message>
+        <source>mm</source>
+        <translation>मिमी</translation>
+    </message>
+    <message>
+        <source>Wind: </source>
+        <translation>हवा: </translation>
+    </message>
+    <message>
+        <source>m/s</source>
+        <translation>मीटर प्रति सेकंड</translation>
+    </message>
+</context>
+<context>
+    <name>WeatherModel</name>
+    <message>
+        <source>Can&apos;t parse the xml file</source>
+        <translation>एक्सएमएल फाइल विश्लेषण में कठिनायी</translation>
+    </message>
+</context>
+<context>
+    <name>utils</name>
+    <message>
+        <source>°C</source>
+        <translation>°C</translation>
+    </message>
+</context>
+</TS>
