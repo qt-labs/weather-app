2a992b14e3e32f77e8cf36474ffbe773950fed79
Nico Vertriest <nico.vertriest@digia.com>
2014-03-07 10:07:28 +0100

    Translation: add Norwegian Bokmål
    Change-Id: I4cc4bc2905071131212de539bacdf193b47a60de
    Reviewed-by: Caroline Chao <caroline.chao@digia.com>

---
+++ translations/QuickForecast_nb.ts
@@ -0,0 +1,132 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="nb_NO">
+<context>
+    <name>ApplicationInfo</name>
+    <message>
+        <source>Error loading image - Host not found or unreachable</source>
+        <translation>Feil ved opplasting av bildet - Server ikke funnet eller kan ikke nås</translation>
+    </message>
+    <message>
+        <source>Waiting for cities, network may be slow...</source>
+        <translation>Henter byer, nettverk sannsynligvis tregt...</translation>
+    </message>
+    <message>
+        <source>Network error: %1</source>
+        <translation>Nettverksfeil: %1</translation>
+    </message>
+</context>
+<context>
+    <name>BasicPage</name>
+    <message>
+        <source>Find City</source>
+        <translation>Søk by</translation>
+    </message>
+</context>
+<context>
+    <name>CitiesListModel</name>
+    <message>
+        <source>no city found</source>
+        <translation>Fant ingen byer</translation>
+    </message>
+</context>
+<context>
+    <name>CitiesPage</name>
+    <message>
+        <source>Cities</source>
+        <translation>Byer</translation>
+    </message>
+    <message>
+        <source>No Cities</source>
+        <translation>Ingen byer</translation>
+    </message>
+</context>
+<context>
+    <name>CityModel</name>
+    <message>
+        <source>Network error: </source>
+        <translation>Nettverksfeil: </translation>
+    </message>
+    <message>
+        <source>No data at given url</source>
+        <translation>Ingen data på den spesifiserte URL-en</translation>
+    </message>
+</context>
+<context>
+    <name>LongTermDayItem</name>
+    <message>
+        <source>m/s</source>
+        <translation>m/s</translation>
+    </message>
+</context>
+<context>
+    <name>LongTermPage</name>
+    <message>
+        <source>10 Day Forecast</source>
+        <translation>10 dagers varsel</translation>
+    </message>
+    <message>
+        <source>Loading data...</source>
+        <translation>Henter data...</translation>
+    </message>
+    <message>
+        <source>Problem loading the data: </source>
+        <translation>Problem ved opplasting av data: </translation>
+    </message>
+</context>
+<context>
+    <name>OneDayPage</name>
+    <message>
+        <source>Today</source>
+        <translation>I dag</translation>
+    </message>
+    <message>
+        <source>Tomorrow</source>
+        <translation>I morgen</translation>
+    </message>
+</context>
+<context>
+    <name>OneDayZoomItem</name>
+    <message>
+        <source>°C</source>
+        <translation type="vanished">°C</translation>
+    </message>
+    <message>
+        <source>Precipitation: </source>
+        <translation>Nedbør: </translation>
+    </message>
+    <message>
+        <source>mm</source>
+        <translation>mm</translation>
+    </message>
+    <message>
+        <source>Wind: </source>
+        <translation>Vind: </translation>
+    </message>
+    <message>
+        <source>m/s</source>
+        <translation>m/s</translation>
+    </message>
+</context>
+<context>
+    <name>WeatherModel</name>
+    <message>
+        <source>Can&apos;t parse the xml file</source>
+        <translation>XML-filen kan ikke leses</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <source>Quick Forecast</source>
+        <translation type="vanished">Quick Forecast</translation>
+    </message>
+</context>
+<context>
+    <name>utils</name>
+    <message>
+        <source>°C</source>
+        <translation>°C</translation>
+    </message>
+</context>
+</TS>
