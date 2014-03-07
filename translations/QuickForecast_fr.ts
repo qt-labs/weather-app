d5646cedc733303a3315c72ffb1549f1a31817d0
Caroline Chao <caroline.chao@digia.com>
2014-03-07 08:26:23 +0100

    Translation: Add French
    Change-Id: I7a5d26bb3bf80a845f49dcc5f86228f0a38e3ca6
    Reviewed-by: Caroline Chao <caroline.chao@digia.com>

---
+++ translations/QuickForecast_fr.ts
@@ -0,0 +1,121 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr_FR">
+<context>
+    <name>ApplicationInfo</name>
+    <message>
+        <source>Error loading image - Host not found or unreachable</source>
+        <translation>Erreur de chargement de l&apos;image - Le serveur ne peut pas être localisé</translation>
+    </message>
+    <message>
+        <source>Waiting for cities, network may be slow...</source>
+        <translation>Patientez pendant le chargement des villes, la connection peut être lente...</translation>
+    </message>
+    <message>
+        <source>Network error: %1</source>
+        <translation>Erreur réseau : %1</translation>
+    </message>
+</context>
+<context>
+    <name>BasicPage</name>
+    <message>
+        <source>Find City</source>
+        <translation>Rechercher une ville</translation>
+    </message>
+</context>
+<context>
+    <name>CitiesListModel</name>
+    <message>
+        <source>no city found</source>
+        <translation>aucune ville n&apos;a été trouvée</translation>
+    </message>
+</context>
+<context>
+    <name>CitiesPage</name>
+    <message>
+        <source>Cities</source>
+        <translation>Villes</translation>
+    </message>
+    <message>
+        <source>No Cities</source>
+        <translation>Aucune ville</translation>
+    </message>
+</context>
+<context>
+    <name>CityModel</name>
+    <message>
+        <source>Network error: </source>
+        <translation>Erreur réseau : </translation>
+    </message>
+    <message>
+        <source>No data at given url</source>
+        <translation>Aucune donnée pour cette url</translation>
+    </message>
+</context>
+<context>
+    <name>LongTermDayItem</name>
+    <message>
+        <source>m/s</source>
+        <translation>m/s</translation>
+    </message>
+</context>
+<context>
+    <name>LongTermPage</name>
+    <message>
+        <source>10 Day Forecast</source>
+        <translation>Prévisions à dix jours</translation>
+    </message>
+    <message>
+        <source>Loading data...</source>
+        <translation>Chargement des données...</translation>
+    </message>
+    <message>
+        <source>Problem loading the data: </source>
+        <translation>Problème lors du chargement des données : </translation>
+    </message>
+</context>
+<context>
+    <name>OneDayPage</name>
+    <message>
+        <source>Today</source>
+        <translation>Aujourd&apos;hui</translation>
+    </message>
+    <message>
+        <source>Tomorrow</source>
+        <translation>Demain</translation>
+    </message>
+</context>
+<context>
+    <name>OneDayZoomItem</name>
+    <message>
+        <source>Precipitation: </source>
+        <translation>Précipitations : </translation>
+    </message>
+    <message>
+        <source>mm</source>
+        <translation>mm</translation>
+    </message>
+    <message>
+        <source>Wind: </source>
+        <translation>Vent : </translation>
+    </message>
+    <message>
+        <source>m/s</source>
+        <translation>m/s</translation>
+    </message>
+</context>
+<context>
+    <name>WeatherModel</name>
+    <message>
+        <source>Can&apos;t parse the xml file</source>
+        <translation>Impossible d&apos;analyser le fichier xml</translation>
+    </message>
+</context>
+<context>
+    <name>utils</name>
+    <message>
+        <source>°C</source>
+        <translation>°C</translation>
+    </message>
+</context>
+</TS>
