7704b93adbd767f61708bf471adb40c11f094d65
Caroline Chao <caroline.chao@digia.com>
2014-03-07 08:27:35 +0100

    Translation: Add Romanian
    Change-Id: I3c5785c61c391a8e5b30580317b987f1bd7e952f
    Reviewed-by: Caroline Chao <caroline.chao@digia.com>

---
+++ translations/QuickForecast_ro.ts
@@ -0,0 +1,121 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ro_RO">
+<context>
+    <name>ApplicationInfo</name>
+    <message>
+        <source>Error loading image - Host not found or unreachable</source>
+        <translation>Eroare incărcare imagine - gazdă inexistentă sau indisponibilă</translation>
+    </message>
+    <message>
+        <source>Waiting for cities, network may be slow...</source>
+        <translation>Aşteptare oraşe, conexiune lentă...</translation>
+    </message>
+    <message>
+        <source>Network error: %1</source>
+        <translation>Eroare de reţea: %1</translation>
+    </message>
+</context>
+<context>
+    <name>BasicPage</name>
+    <message>
+        <source>Find City</source>
+        <translation>Găseşte oraş</translation>
+    </message>
+</context>
+<context>
+    <name>CitiesListModel</name>
+    <message>
+        <source>no city found</source>
+        <translation>Niciun oraş găsit</translation>
+    </message>
+</context>
+<context>
+    <name>CitiesPage</name>
+    <message>
+        <source>Cities</source>
+        <translation>Oraşe</translation>
+    </message>
+    <message>
+        <source>No Cities</source>
+        <translation>Niciun oraş</translation>
+    </message>
+</context>
+<context>
+    <name>CityModel</name>
+    <message>
+        <source>Network error: </source>
+        <translation>Eroare de reţea: </translation>
+    </message>
+    <message>
+        <source>No data at given url</source>
+        <translation>Date inexistente la url-ul indicat</translation>
+    </message>
+</context>
+<context>
+    <name>LongTermDayItem</name>
+    <message>
+        <source>m/s</source>
+        <translation>m/s</translation>
+    </message>
+</context>
+<context>
+    <name>LongTermPage</name>
+    <message>
+        <source>10 Day Forecast</source>
+        <translation>Prognoza pe 10 zile</translation>
+    </message>
+    <message>
+        <source>Loading data...</source>
+        <translation>Datele se incarcă...</translation>
+    </message>
+    <message>
+        <source>Problem loading the data: </source>
+        <translation>Eroare incărcare date: </translation>
+    </message>
+</context>
+<context>
+    <name>OneDayPage</name>
+    <message>
+        <source>Today</source>
+        <translation>Azi</translation>
+    </message>
+    <message>
+        <source>Tomorrow</source>
+        <translation>Mâine</translation>
+    </message>
+</context>
+<context>
+    <name>OneDayZoomItem</name>
+    <message>
+        <source>Precipitation: </source>
+        <translation>Precipitaţii: </translation>
+    </message>
+    <message>
+        <source>mm</source>
+        <translation>mm</translation>
+    </message>
+    <message>
+        <source>Wind: </source>
+        <translation>Vânt: </translation>
+    </message>
+    <message>
+        <source>m/s</source>
+        <translation>m/s</translation>
+    </message>
+</context>
+<context>
+    <name>WeatherModel</name>
+    <message>
+        <source>Can&apos;t parse the xml file</source>
+        <translation>Fişierul xml nu poate fi parsat</translation>
+    </message>
+</context>
+<context>
+    <name>utils</name>
+    <message>
+        <source>°C</source>
+        <translation>°C</translation>
+    </message>
+</context>
+</TS>
